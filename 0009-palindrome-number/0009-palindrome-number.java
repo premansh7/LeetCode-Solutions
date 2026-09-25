037976e3dab739408e036698de76945444fbb093
premanshu <63599830+premansh7@users.noreply.github.com>
2026-09-25 20:03:28 +0530

    Time: 5 ms (83.96%), Space: 45.9 MB (52.65%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int original = x;
+       while(x>0){
+        int digit=x%10;
+        rev= rev*10+digit;
+        x /= 10;
+        }
+        if(rev == original) return true;
+        else return false; 
+    }
+}
